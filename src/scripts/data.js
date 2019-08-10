2b2e089e4ea617317897ecf4d8219aac6e264387
Kelly Coles <kellycoles1@gmail.com>
2019-08-09 22:14:13 -0500

    radio button part 10000000 fail but styled output

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -9,19 +9,36 @@
 
 const API = {
     getJournalData() {
-        return fetch("http://localhost:8088/entries?_sort=id&_order=desc") // Fetch from the API
+        return fetch("http://localhost:3000/entries?_sort=id&_order=desc") // Fetch from the API
             .then(response => response.json())        // Parse as JSON
 
     },
 
     saveJournalEntries(entryObject) {
-        return fetch("http://localhost:8088/entries", {
+        return fetch("http://localhost:3000/entries", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(entryObject)
         })
-    }
+    },
+
+  filterJournalEntries(mood) {
+    return fetch(`http://localhost:3000/entries?mood=${mood}`)
+    .then(entries => entries.json())
+  },
+
+  deleteJournalEntry(deleteBtnId) {
+    return fetch(`http://localhost:3000/entries/${deleteBtnId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  }
 }
+ 
+    
+
 export default API;
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,28 +2,31 @@
     Main application logic that uses the functions and objects
     defined in the other JavaScript files. 
  ===================================================================*/
- import API from "./data.js"
- import journalEntry from "./entriesDom.js"
+import API from "./data.js"
+import journalEntry from "./entriesDom.js"
 //  import entryLogOutput from "./entriesDom.js"
 
- // sets date to current date
- document.querySelector("#journalDate").valueAsDate = new Date();
+// sets date to current date
+document.querySelector("#journalDate").valueAsDate = new Date();
 
 // Reference DOM elements
 const entryLogOutput = document.querySelector("#entryLog")
 
-const journalDateInput= document.querySelector("#journalDate")
+const journalDateInput = document.querySelector("#journalDate")
 const journalConceptInput = document.querySelector("#journalConcepts")
 const conceptEntryInput = document.querySelector("#conceptDetails")
 const moodInput = document.querySelector(".mood")
 const submit = document.querySelector("#submit")
+const radioButton = document.querySelector(".radioButtons")
 
 //Do something with entries
 const getAndRender = () => {
-    entryLogOutput.innerHTML =""
-API.getJournalData().then((parsedEntries) => {journalEntry.sendToFactory(parsedEntries)})
+    entryLogOutput.innerHTML = ""
+    API.getJournalData().then((parsedEntries) => { journalEntry.sendToFactory(parsedEntries) })
+
 }
-getAndRender ()
+getAndRender()
+
 
 // Create an object with a new journal entry
 const createEntryObject = (date, concepts, entry, mood) => {
@@ -37,13 +40,12 @@ const createEntryObject = (date, concepts, entry, mood) => {
 }
 
 submit.addEventListener("click", (event) => {
-  
-    console.log("click")
     // Check for empty string
-    if( journalConceptInput.value === "" ||  conceptEntryInput.value ==="" || moodInput.value === "") {
+    if (journalConceptInput.value === "" || conceptEntryInput.value === "" || moodInput.value === "") {
         alert("Fill out the journal, Kelly")
-        
+        event.preventDefault();
     }
+
     // call createEntryObject and send it the form data. It will make a new object with the current form data. 
     const newJournalEntry = createEntryObject(journalDateInput.value, journalConceptInput.value, conceptEntryInput.value, moodInput.value)
     //call the API method saveJournalEntries from data.js and pass it the new entry.
@@ -55,4 +57,18 @@ submit.addEventListener("click", (event) => {
     moodInput.value = ""
 
 })
+
+
+radioButton.addEventListener("click", (event) => {
+    if (event.target.className == "radioButton--item") {
+        const mood = event.target.value
+        console.log(mood)
+        entryLogOutput.innerHTML = ""
+        API.filterJournalEntries(mood).then((parsedEntries) => {journalEntry.sendToFactory(parsedEntries)})
+
+
+    }
+
+})
+
 
